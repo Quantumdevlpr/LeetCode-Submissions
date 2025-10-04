9f75dbbbdda60cf552e9189a9179d30bd6c7cbcf
Shivansh <151555468+Quantumdevlpr@users.noreply.github.com>
2025-10-04 12:43:42 +0530

    Time: 46 ms (56.65%) | Memory: 112.7 MB (80.49%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+class Node {
+    public int key, val;
+    public Node next, prev;
+
+    Node() {
+        key = val = -1;
+        next = prev = null;
+    }
+
+    Node(int k, int value) {
+        key = k;
+        val = value;
+        next = prev = null;
+    }
+}
+
+class LRUCache {
+    private Map<Integer, Node> map;
+    private int capacity;
+    private Node head, tail;
+
+    private void deleteNode(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+    }
+
+    private void insertAfterHead(Node node) {
+        Node nextNode = head.next;
+        node.prev = head;
+        node.next = nextNode;
+        head.next = node;
+        nextNode.prev = node;
+    }
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        Node node = map.get(key);
+        int val = node.val;
+        deleteNode(node);
+        insertAfterHead(node);
+        return val;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.val = value;
+            deleteNode(node);
+            insertAfterHead(node);
+            return;
+        }
+
+        if (map.size() == capacity) {
+            Node node = tail.prev;
+            map.remove(node.key);
+            deleteNode(node);
+        }
+
+        Node newNode = new Node(key, value);
+        map.put(key, newNode);
+        insertAfterHead(newNode);
+    }
+}
