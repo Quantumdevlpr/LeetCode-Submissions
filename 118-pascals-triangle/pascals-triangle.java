f4060646d58e25193cdd4ca9b694c7404fb906ff
Shivansh <151555468+Quantumdevlpr@users.noreply.github.com>
2026-04-12 17:54:38 +0530

    Time: 1 ms (95.46%) | Memory: 43.6 MB (22.26%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,13 +1,19 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-    List<List<Integer>> result = new ArrayList<>();
-    for(int i = 0; i < numRows ; i++){
-      List<Integer> row = new ArrayList<Integer>(Collections.nCopies(i + 1 ,1 ));
-      for(int j = 1 ; j < i ;j++){
-        row.set(j , result.get(i-1).get(j-1)+ result.get(i-1).get(j));
-      }
-      result.add(row);
-    }
-    return result;
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            int curr = 1;
+
+            for (int j = 0; j <= i; j++) {
+                row.add(curr);
+                curr = curr * (i - j) / (j + 1);
+            }
+
+            result.add(row);
+        }
+
+        return result;
     }
 }
