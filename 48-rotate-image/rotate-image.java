a7cd581dd70d694138bcbc482c3ad567c5e456fc
Shivansh <151555468+Quantumdevlpr@users.noreply.github.com>
2026-04-12 19:28:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (58.91%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+    //sbse phle transpose lenge is matrx ka like row -> column
+    for(int i= 0; i < matrix.length ; i++){
+      for(int j = i ; j < matrix.length ; j++){
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }   
+    //ab saari rows ko reverse krdo for the result 
+    for(int i= 0 ; i < matrix.length ; i++){
+      int left = 0;
+      int right = matrix.length-1;
+      while(left < right){
+        int temp = matrix[i][left];
+        matrix[i][left] = matrix[i][right];
+        matrix[i][right] = temp;
+        left++;
+        right--;
+      }
+    }
+    }
+}
