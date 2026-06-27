f16e9fab0511dd6375e40e255cbbaa9ebe0045c3
Shivansh <151555468+Quantumdevlpr@users.noreply.github.com>
2026-06-27 10:40:50 +0530

    Time: 1 ms (98.57%) | Memory: 47.5 MB (39.88%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,17 +1,30 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
-           int sum = 0;
-       for(int i=0; i<nums.length;i++){
-        sum += nums[i];
-       } 
-       int leftsum =0;
-       for(int i =0; i<nums.length;i++){
-        if(leftsum == (sum - leftsum - nums[i])){
-            return i;
+    public int pivotIndex(int[] arr) {
+     int n = arr.length;
+
+        int[] pref = new int[n];
+        int[] suff = new int[n];
+
+        // Initialize the ends of prefix
+        // and suffix array
+        pref[0] = arr[0];
+        suff[n - 1] = arr[n - 1];
+
+        // Calculate prefix sum for all indices
+        for (int i = 1; i < n; i++) 
+            pref[i] = pref[i - 1] + arr[i];
+
+        // Calculating suffix sum for all indices
+        for (int i = n - 2; i >= 0; i--) 
+            suff[i] = suff[i + 1] + arr[i];
+
+        // Checking if prefix sum 
+        // is equal to suffix sum
+        for (int i = 0; i < n; i++) {
+            if (pref[i] == suff[i]) 
+                return i;
         }
-        leftsum+=nums[i];
-       }
-       return -1;
-  
+
+        return -1;    
     }
 }
