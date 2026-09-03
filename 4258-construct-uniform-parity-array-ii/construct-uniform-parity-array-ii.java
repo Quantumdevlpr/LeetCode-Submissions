049795c33aef14693dc6575575263e1b80745a7b
Shivansh <151555468+Quantumdevlpr@users.noreply.github.com>
2026-09-03 10:41:17 +0530

    Time: 2 ms (100.00%) | Memory: 121.7 MB (44.20%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+      int n = nums1.length;
+      int min = Integer.MAX_VALUE;
+      for(int i = 0 ; i < n ; i++){
+        if(nums1[i] < min){
+          min = nums1[i];
+        }
+      }
+      if(min % 2 != 0){
+        return true;
+      }
+      for(int i = 0 ; i < n ;i++){
+        if(nums1[i] %2 != 0){
+          return false;
+        }
+      }
+      return true;
+    }
+}
